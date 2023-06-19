344167738232e03bf9de871456d6e87f6bf755cf
hyouka0904 <charles.kp0904@gmail.com>
2023-06-19 22:00:14 +0800

    fix test

--- src/policy/test.hpp
+++ src/policy/test.hpp
@@ -6,7 +6,7 @@
  * @brief Policy class for random policy, 
  * your policy class should have get_move method
  */
-class Random{
+class Test{
 public:
   static Move get_move(State *state, int depth);
 };
