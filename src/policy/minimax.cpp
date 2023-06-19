a5695a7e880bc2ffbcf18a9efa277892bbf3294a
hyouka0904 <charles.kp0904@gmail.com>
2023-06-19 22:53:00 +0800

    tried to fix there is no best move

--- src/policy/minimax.cpp
+++ src/policy/minimax.cpp
@@ -17,14 +17,17 @@ Move minimax::get_move(State *state, int depth){
         state->get_legal_actions();
     Move BestMove;
     int val = -900000;
+    bool existed_bestmove = false;
     for(auto it : state->legal_actions){
         int nxt_val = minimax::minimax_val(state->next_state(it), depth-1, !state->player);
         if(nxt_val>val) {
             val = nxt_val;
             BestMove = it;
+            existed_bestmove = true;
         }
     }
-    return BestMove;
+    if(existed_bestmove)    return BestMove;
+    else return state->legal_actions[(rand()+depth)%state->legal_actions.size()];
 }
 int minimax::minimax_val(State* state, int depth, int player){
     if(!state->legal_actions.size())
